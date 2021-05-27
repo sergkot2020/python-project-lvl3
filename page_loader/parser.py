fed1d4cc98d4c4b27b61383243091d8f1f8efe14
snovozhilov <sergey.novozhilov@upm.ai>
2021-05-27 23:42:09 +0300

    fix: add -o in parser.py

--- page_loader/parser.py
+++ page_loader/parser.py
@@ -8,7 +8,7 @@ from typing import Tuple
 def parse() -> Tuple[str, str]:
     """Run script."""
     parser = argparse.ArgumentParser(description='page-loader')
-    parser.add_argument('--output', type=str, default=os.getcwd())
+    parser.add_argument('-f', '--output', type=str, default=os.getcwd())
     parser.add_argument('url', type=str)
     args = parser.parse_args()
     return args.url, args.output
